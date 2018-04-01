ca5f5b0a2062ef7ce9c9399041cc6b8f8cf5136b
Craig Perks <craigperks@gmail.com>
2018-04-01 21:47:59 +0100

    First commit

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/lib/Model/Ship.php';
+require_once __DIR__ . '/lib/Service/BattleManager.php';
+require_once __DIR__ . '/lib/Service/ShipLoader.php';
+require_once __DIR__ . '/lib/Model/BattleResult.php';
+require_once __DIR__ . '/lib/Service/Container.php';
+
+// Made up details that I would only ever use locally because I'm not in the habit of sharing my details online!
+$configuration = array(
+    'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'craigweb',
+    'db_pass' => 'super8'
+);
---
+++ lib/Model/Ship.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Class Ship: Model class (models something - like a ship)
+ */
+class Ship
+{
+
+    private $id;
+
+    private $name;
+
+    private $weaponPower = 0;
+
+    private $jediFactor = 0;
+
+    private $strength = 0;
+
+    private $underRepair;
+
+    public function __construct($name) {
+        $this->name = $name;
+        $this->underRepair = mt_rand(1, 100) < 30;
+    }
+
+    public function isFunctional() {
+        return !$this->underRepair;
+    }
+
+    public function sayHello() {
+        echo 'Hello';
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getNameAndSpecs($useShortFormat = false) {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+        else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+
+    }
+
+    public function doesGivenShipHaveMoreStrength($givenShip) {
+        return $givenShip->strength > $this->strength;
+    }
+
+    public function setStrength($strength) {
+        if (!is_numeric($strength)) {
+            throw new Exception('Invalid strength passed ' . $strength);
+        }
+
+        $this->strength = $strength;
+    }
+
+    public function getStrength() {
+        return $this->strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower(): int
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower(int $weaponPower): void
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJediFactor(): int
+    {
+        return $this->jediFactor;
+    }
+
+    /**
+     * @param int $jediFactor
+     */
+    public function setJediFactor(int $jediFactor): void
+    {
+        $this->jediFactor = $jediFactor;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+
+}
